aa11b5d2da743a3f440f5b007428c907c0e975b9
Stephen Thomas <stevekwthomas@gmail.com>
2020-12-03 14:39:28 +0000

    show me another random thing button working

--- src/App.js
+++ src/App.js
@@ -1,19 +1,10 @@
 import "./App.css";
 import firebase from "./firebase.js";
 import { Component } from "react";
-import { randomNumber } from "./randomizers.js";
-import DisplayRandomGoal from "./DisplayRandomGoal.js";
 import DisplayForm from "./DisplayForm.js";
-// import getRandomGoal from "./getRandomGoal.js";
-// 1. configure firebase on react --
-// 2. connect firebase data to the state --
-// 3. map through the 'goal' state to display each 'goal' --
-// 4. add remove button
-// 5. on remove button click, remove the 'goal' that was clicked on, remove it from firebase
-// 6. create input form, to get user input value
-// 7. on submit of form, submit user input value to firebase
+
 class App extends Component {
-  generatedNumber = randomNumber();
+
   constructor(props) {
     super();
     this.state = {
@@ -24,23 +15,27 @@ class App extends Component {
     this.updateFirebase = this.updateFirebase.bind(this);
     this.removeFromFirebase = this.removeFromFirebase.bind(this);
   }
+
   // get reference to firebase and return it
   firebaseRef() {
     const dbRef = firebase.database().ref();
     return dbRef;
   }
+
   //generic function to update firebase
   updateFirebase(value) {
     if (value === undefined || null) return;
     const dbRef = this.firebaseRef();
     dbRef.push(value);
   }
+
   //generic function to remove from firebase
   removeFromFirebase(id) {
     if (id === undefined || null) return;
     const dbRef = this.firebaseRef();
     dbRef.child(id).remove();
   }
+
   componentDidMount() {
     const dbRef = this.firebaseRef();
     dbRef.on("value", (snapshot) => {
@@ -78,25 +73,18 @@ class App extends Component {
     return (
       <div>
         <h1>Essay Idea DB</h1>
-        {/* DISPLAY FORM HERE */}
+
         <DisplayForm
           goalsArray={this.state.goalsArray}
           updateFirebase={this.updateFirebase}
           removeFromFirebase={this.removeFromFirebase}
           displayRandomThing={this.state.randomGoal}
-          // randomGoal={getRandomGoal(this.state.goalsArray)}
+          getRandomGoal={this.getRandomGoal}
         />
-        {/* <div className="mainDisplayGoal">
-          <p>
-            Random number: <strong>{this.generatedNumber}</strong>
-          </p>
-          <p>
-            And from the external component:
-            <DisplayRandomGoal />
-          </p>
-        </div> */}
+        
       </div>
     );
   }
 }
+
 export default App;
--- src/DisplayForm.js
+++ src/DisplayForm.js
@@ -1,15 +1,16 @@
 import "./App.css";
-import firebase from "./firebase.js";
+// import firebase from "./firebase.js";
 import { Component } from "react";
 class DisplayForm extends Component {
   constructor(props) {
     super(props);
-    // this.randomGoal = this.props.randomGoal();
     this.state = {
       userInput: "",
     };
   }
+
   componentDidMount() {}
+
   handleSubmit = (e) => {
     e.preventDefault();
     // destructure firebase function object from this.props
@@ -18,16 +19,19 @@ class DisplayForm extends Component {
     // clear the text input field
     document.querySelector("input").value = "";
   };
+
   handleInputChange = (e) => {
     this.setState({
       userInput: e.target.value,
     });
   };
+
   removeGoal = (goalId) => {
     if (!goalId) return;
     const { removeFromFirebase } = this.props;
     removeFromFirebase(goalId);
   };
+
   renderGoals = (goalsArray) => {
     if (!goalsArray.length) return <li>No goals to show</li>;
     return goalsArray.map(({ id, value }) => {
@@ -39,23 +43,29 @@ class DisplayForm extends Component {
       );
     });
   };
+
   render() {
     return (
       <div>
+        
+        {/* // FORM */}
         <form action="">
           <label htmlFor="newGoal">What do you want someone to write about? </label>
           <input type="text" id="newGoal" onChange={this.handleInputChange} />
           <button onClick={this.handleSubmit}>Add</button>
         </form>
-        {/* This should be refactored to a button with text next to it, to handle fetching a new random goal IE onclick method  */}
 
-        {<p>My random goal for today is: {this.randomGoal}</p>}
+        {/* // RANDOM ITEM */}
 
-        <p>My random goal for today 2 is: {this.props.displayRandomThing}</p>
+        <p>Random Essay Idea: <strong>{this.props.displayRandomThing}</strong> 
+            <button onClick={this.props.getRandomGoal}>Show me another</button> 
+        </p>
 
-        {this.renderGoals(this.props.goalsArray)}
-        {/* <QuestionForm /> */}
-        <ul>{}</ul>
+        {/* // LIST OF ITEMS */}
+        <ul>
+            {this.renderGoals(this.props.goalsArray)}
+        </ul>
+
       </div>
     );
   }
